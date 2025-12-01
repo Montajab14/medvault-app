f67271b783c4c432c13fb1cbf8076d06c4c61ff2
Montajab14 <montajabmarouf14@gmail.com>
2025-12-01 15:40:32 +0100

    Adding modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PatientsModule } from './patients/patients.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PatientsModule, CryptoModule],
   controllers: [AppController],
   providers: [AppService],
 })
